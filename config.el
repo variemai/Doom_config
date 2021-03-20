3a13b71be2462ef5ce4d7c074a87759a9bdda206
variemai <vardas@ics.forth.gr>
2021-03-20 03:15:40 +0200

    Latest

--- config.el
+++ config.el
@@ -108,7 +108,8 @@
 (setq org-latex-to-pdf-process
   '("xelatex -interaction nonstopmode %f"
      "xelatex -interaction nonstopmode %f")) ;; for multiple passes
-
+(setq org-latex-pdf-process (list
+   "latexmk -pdflatex='lualatex -shell-escape -interaction nonstopmode' -pdf -f  %f"))
 ;; (defvar my-term-shell "/bin/zsh")
 ;;   (defadvice ansi-term (before force-bash)
 ;;     (interactive (list my-term-shell)))
@@ -334,3 +335,55 @@
 ;; (global-set-key (kbd "C-x b") 'helm-mini)
 ;; (setq helm-M-x-fuzzy-match t) ;; optional fuzzy matching for helm-M-x
 ;; (add-hook 'org-mode-hook (lambda () (org-bullets-mode)))
+
+;; #+LaTeX_CLASS: beamer in org files
+;; (unless (boundp 'org-export-latex-classes)
+;;   (setq org-export-latex-classes nil))
+;; (add-to-list 'org-export-latex-classes
+;;   ;; beamer class, for presentations
+;;   '("beamer"
+;;      "\\documentclass[11pt]{beamer}\n
+;;       \\mode<{{{beamermode}}}>\n
+;;       \\usetheme{{{{beamertheme}}}}\n
+;;       \\usecolortheme{{{{beamercolortheme}}}}\n
+;;       \\beamertemplateballitem\n
+;;       \\setbeameroption{show notes}
+;;       \\usepackage[utf8]{inputenc}\n
+;;       \\usepackage[T1]{fontenc}\n
+;;       \\usepackage{hyperref}\n
+;;       \\usepackage{color}
+;;       \\usepackage{listings}
+;;       \\lstset{numbers=none,language=[ISO]C++,tabsize=4,
+;;   frame=single,
+;;   basicstyle=\\small,
+;;   showspaces=false,showstringspaces=false,
+;;   showtabs=false,
+;;   keywordstyle=\\color{blue}\\bfseries,
+;;   commentstyle=\\color{red},
+;;   }\n
+;;       \\usepackage{verbatim}\n
+;;       \\institute{{{{beamerinstitute}}}}\n
+;;        \\subject{{{{beamersubject}}}}\n"
+
+;;      ("\\section{%s}" . "\\section*{%s}")
+
+;;      ("\\begin{frame}[fragile]\\frametitle{%s}"
+;;        "\\end{frame}"
+;;        "\\begin{frame}[fragile]\\frametitle{%s}"
+;;        "\\end{frame}")))
+
+;;   ;; letter class, for formal letters
+
+;;   (add-to-list 'org-export-latex-classes
+
+;;   '("letter"
+;;      "\\documentclass[11pt]{letter}\n
+;;       \\usepackage[utf8]{inputenc}\n
+;;       \\usepackage[T1]{fontenc}\n
+;;       \\usepackage{color}"
+
+;;      ("\\section{%s}" . "\\section*{%s}")
+;;      ("\\subsection{%s}" . "\\subsection*{%s}")
+;;      ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+;;      ("\\paragraph{%s}" . "\\paragraph*{%s}")
+;;      ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
