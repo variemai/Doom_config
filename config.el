2d4a69ad307d6ff9d0b9966ea314e1b1a27f699e
variemai <vardas@ics.forth.gr>
2021-04-18 12:35:14 +0300

    Lets pump this shit up

--- config.el
+++ config.el
@@ -24,12 +24,12 @@
   :hook (org-mode . org-bullets-mode))
 
 ;; (setq org-bullets-mode 1)
-(setq doom-font (font-spec :family "JetBrains Mono" :size 18 :weight 'Medium)
-      doom-variable-pitch-font (font-spec :family "JetBrains Mono" :size 18))
+(setq doom-font (font-spec :family "IBM Plex Mono SemiBold" :size 19)
+      doom-variable-pitch-font (font-spec :family "IBM Plex Mono SemiBold" :size 19))
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-vibrant)
+ (setq doom-theme 'doom-vibrant)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
